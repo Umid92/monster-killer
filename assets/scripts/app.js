cc8b38129f448093e5af294384b2d20ebda895c2
Umid R. Karimov (EHS) <Umid.Karimov@local.pmdhospitality.az>
2022-11-14 16:23:13 +0400

    strong attack added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,39 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 15;
+const STRONG_ATTACK_VALUE = 17;
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  let damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === "ATACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  let damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
+  let playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth >= 0) {
+    alert("YOU WON");
+  } else if (currentMonsterHealth >= 0 && currentPlayerHealth <= 0) {
+    alert("YOU LOST");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("DRAW");
+  }
+}
+function attackHandler() {
+  attackMonster("ATACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATACK");
 }
 
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
